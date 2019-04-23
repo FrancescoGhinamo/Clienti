22592d34050680fb02827810355e5cfd24034d54
pex9 <davidepellegrino88@gmail.com>
2019-04-23 23:38:14 +0200

    up

--- src/frontend/InterfacciaGraficaCliente.java
+++ src/frontend/InterfacciaGraficaCliente.java
@@ -52,47 +52,7 @@ public class InterfacciaGraficaCliente extends JFrame implements ActionListener 
 		testo = new JTextArea();
 		add(testo);
 	}
-	public void caricaeLetturaPagina(String url)
-	{
-		//https://www.claudiogarau.it/Java/leggere-una-pagina-web-con-java.php
-		//legge tutto della pagina, bisogna pulirlo dalle robe inutili
-		  URL cg = null;
-		try {
-			cg = new URL("http://www.claudiogarau.it");
-		} catch (MalformedURLException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-	        BufferedReader in = null;
-			try {
-				in = new BufferedReader(
-				new InputStreamReader(cg.openStream()));
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-	        String inputLine;
-	        if(testo.getText()!="")
-	        {
-	        	testo.setText("");
-	        }
-	        try {
-				while ((inputLine = in.readLine()) != null)
-				    testo.append(inputLine);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        try {
-				in.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        
-	        
-	}
+	
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource().equals(inserireIndirizzo))
