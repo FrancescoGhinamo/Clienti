d384e16d7ffc34dbc1e22a6afff2e3bdae1c2cb2
pex9 <davidepellegrino88@gmail.com>
2019-04-26 18:52:49 +0200

    up

--- src/backend/Clients.java
+++ src/backend/Clients.java
@@ -2,6 +2,7 @@ package backend;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -9,6 +10,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import frontend.InterfacciaGraficaCliente;
+import server.backend.service.serverService.ByteServerServiceFactory;
 
 public class Clients implements Runnable {
 	private Socket server=null;
@@ -44,15 +46,16 @@ public class Clients implements Runnable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//server genera la pagina e mi restitusce la roba(gia convertita oppure solo la pagina come vuoi tu :)
-			try {
-				
-				String ricevuta =input.readLine();
-				i.setMessaggioRicevuto(ricevuta);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			byte[] ris=ByteServerServiceFactory.getByteServerService().leggiByteIngresso(server);
+			char[] chars = new char[ris.length];
+			for(int i = 0; i < ris.length; i++) {
+				chars[i] = (char) ris[i];
 			}
+			
+			//File file = null;
+			String indirizzo = String.valueOf(chars);
+			i.getTesto().setText(indirizzo);
+			
 		}
 		
 		
